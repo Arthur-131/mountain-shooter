5d8728e30a3102a47473fea41002db56b469d616
Arthur-131 <161372807+Arthur-131@users.noreply.github.com>
2026-02-26 14:26:21 -0300

    aula 6 - adicionando colisões de tiros e hitboxes

--- code/Const.py
+++ code/Const.py
@@ -4,6 +4,8 @@ import pygame
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -19,7 +21,7 @@ ENTITY_SPEED = {
     'Player1': 3,
     'Player2': 3,
     'Player1Shot': 4,
-    'Player2Shot': 4,
+    'Player2Shot': 3,
     'Enemy1': 2,
     'Enemy2': 1,
     'Enemy1Shot': 3,
@@ -49,11 +51,57 @@ ENTITY_HEALTH = {
     'Enemy2Shot': 1,
 }
 
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Player1': 1,
+    'Player2': 1,
+    'Player1Shot': 25,
+    'Player2Shot': 20,
+    'Enemy1': 2,
+    'Enemy1Shot': 20,
+    'Enemy2': 2,
+    'Enemy2Shot': 15,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Player1': 0,
+    'Player2': 0,
+    'Player1Shot': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy2': 125,
+    'Enemy1Shot': 0,
+    'Enemy2Shot': 0,
+}
+
 ENTITY_SHOT_DELAY = {
     'Player1': 20,
-    'Player2': 20,
+    'Player2': 15,
     'Enemy1': 100,
-    'Enemy2': 100,
+    'Enemy2': 200,
 }
 
 # M
